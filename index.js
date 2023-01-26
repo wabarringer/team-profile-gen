e8663ee08556ae1d50c1e5ceef45f9c9c7e3856e
William <wabarringer@gmail.com>
2023-01-26 12:30:49 -0800

    Added classes and linked to Employee

---
+++ index.js
@@ -0,0 +1,9 @@
+// TODO: Create inquirer prompt to gathe team members and their information
+// TODO: Generate HTML file
+// TODO: Link email address to default email application with email address populated
+// TODO: GitHub username links to their profile in new tab
+// TODO: Prompt user to enter the team manager’s name, employee ID, email address, and office number
+// TODO: Once inputs are made prompt user with menu with the option to add an engineer or an intern or to finish
+// TODO: Prompt to enter the engineer’s name, ID, email, and GitHub username, then return to menu
+// TODO: Prompt to enter the intern’s name, ID, email, and school,  then return to menu
+// TODO: When finish is selected, exit the application, and the HTML is generated
